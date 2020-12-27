929430148d514647aebbc4ae65a202b94ea7e946
narxiss24 <narxiss@yahoo.com>
2020-12-27 10:18:30 -0500

    auto-push

--- blank.R
+++ blank.R
@@ -1,3 +1,26 @@
 circarr  <- read.csv("https://raw.githubusercontent.com/narxiss24/datasets/master/circarr.csv")
 
-circarr$clinseiz
+names(circarr)
+# [1] "vsd"      "dhca"     "minutes"  "birthwt"  "age"      "clinseiz" "eegseiz"  "pdi"     
+
+table(circarr$vsd)
+#   0   1 
+# 129  42 
+
+table(circarr$clinseiz)
+#   0   1 
+# 159  11 
+
+fisher.test(circarr$vsd, circarr$clinseiz)
+ 
+# 	Fisher's Exact Test for Count Data
+# 
+# data:  circarr$vsd and circarr$clinseiz
+# p-value = 0.02466
+# alternative hypothesis: true odds ratio is not equal to 1
+# 95 percent confidence interval:
+#   1.003864 18.529698
+# sample estimates:
+# odds ratio 
+#   4.205616 
+ 
